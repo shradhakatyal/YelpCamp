360c0193f189d27da41a99461d355f8e91b7745b
shradhakatyal <skatyal2204@gmail.com>
2018-11-18 00:31:12 +0530

    db connected

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
 
 const app = express();
 const port = 5000;
 
-let campgrounds = [
-    {name: "Salmon Creek", image: "http://campingsehnsucht-lwl-blog.de/wp-content/uploads/sites/517/2018/02/best-camping-spots-in-pa-6-624x435.jpg"},
-    {name: "Granite Hill", image: "https://www.planetware.com/photos-large/USSD/south-dakota-black-hills-national-forest-horsethief-lake-campground-tent-site.jpg"},
-    {name: "Mountain Goats Rest", image: "https://www.nationalparks.nsw.gov.au/-/media/npws/images/parks/munmorah-state-conservation-area/freemans-campground/freemans-campground-03.jpg"}
-]
+// Setting up the Schema
+
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,15 +25,30 @@ app.get('/', (req, res) => {
 
 // Getting all the camp grounds
 app.get('/campgrounds', (req, res) => {
-    res.render("campgrounds", {campgrounds});
+    Campground.find({}, (err, campgrounds) => {
+        if(err) {
+            res.json({err});
+        } else {
+            res.render("campgrounds", {campgrounds});
+        }
+    });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render("new");
 });
 
 app.post('/campgrounds', (req, res) => {
     let name = req.body.name;
     let image = req.body.image;
     let campground = {name, image};
-    campgrounds.push(campground);
-    res.redirect("/campgrounds");
+    Campground.create(campground, (err, cg) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    })
 });
 
 app.listen(port, () => console.log(`Yelp Camp is running on http://127.0.0.1:${port}`));
